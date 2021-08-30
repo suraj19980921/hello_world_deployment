38db1aa3f43cba17bd423b6acbfe12e2b9dbc882
Suraj Kumar <sk9966332@gmail.com>
2021-08-30 12:37:12 +0530

    adding django heroku config

--- hello_world_deployment/settings.py
+++ hello_world_deployment/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from decouple import config
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
