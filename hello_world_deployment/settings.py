b4f62ba2418b7431865dfe51b7ec29388130ea97
Suraj Kumar <sk9966332@gmail.com>
2021-08-30 10:50:01 +0530

    adding environment variables

--- hello_world_deployment/settings.py
+++ hello_world_deployment/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@pq^a82q@sc3mjutg#46$et&r6#s0%*fa1^fp=q3=7%l+!p)4l'
+SECRET_KEY = config('SECRET_KEY', default = '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in config('ALLOWED_HOSTS', default='').split(',')
+]
 
 
 # Application definition
